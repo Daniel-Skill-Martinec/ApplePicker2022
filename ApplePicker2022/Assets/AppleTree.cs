4a0c0f2aded96b323e68621144b9c7d75526a133
Daniel-Skill-Martinec <danielpokecraft@gmail.com>
2022-09-13 20:27:22 -0400

    Check In 9/13/22

--- ApplePicker2022/Assets/AppleTree.cs
+++ ApplePicker2022/Assets/AppleTree.cs
@@ -24,12 +24,41 @@ public class AppleTree : MonoBehaviour
     void Start()
     {
         //Start Dropping Apples
+        Invoke("DropApple", 2f);
+
+    }
+
+    void DropApple()
+    {
+        GameObject apple = Instantiate<GameObject>(applePrefab);
+        apple.transform.position = transform.position;
+        Invoke("DropApple", appleDropDelay);
     }
 
     // Update is called once per frame
     void Update()
     {
         //Basic Movement
+        Vector3 pos = transform.position;
+        pos.x += speed * Time.deltaTime;
+        transform.position = pos;
+
         //Changing Direcions
+        if (pos.x < -leftAndRightEdge)
+        {
+            speed = Mathf.Abs(speed);//move right
+        } else if (pos.x > leftAndRightEdge)
+        {
+            speed = -Mathf.Abs(speed);//move left
+        }
+        Debug.Log(pos.x);
+    }
+
+    private void FixedUpdate()
+    {
+        if (Random.value < ChangeDirChance)
+        {
+            speed *= -1;
+        }
     }
 }
